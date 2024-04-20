5fd3ce6cb5ef03362d96471bb3974eaa9b232d97
Balázs Cseke <csebalas2003@gmail.com>
2024-04-20 17:27:29 +0200

    Adatfeltöltés és lisbox OK

--- UserMaintance/UserMaintance/Form1.Designer.cs
+++ UserMaintance/UserMaintance/Form1.Designer.cs
@@ -85,6 +85,7 @@
             button1.TabIndex = 5;
             button1.Text = "button1";
             button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // Form1
             // 
--- UserMaintance/UserMaintance/Form1.cs
+++ UserMaintance/UserMaintance/Form1.cs
@@ -1,18 +1,36 @@
+using System.ComponentModel;
+using UserMaintance.Entities;
+
 namespace UserMaintance
 {
     public partial class Form1 : Form
     {
+        BindingList<User> users = new BindingList<User>();
         public Form1()
         {
             InitializeComponent();
             label1.Text = Resource1.LastName; // label1
             label2.Text = Resource1.FirstName; // label2
             button1.Text = Resource1.Add; // button1
+
+            listBox1.DataSource = users;
+            listBox1.ValueMember = "ID";
+            listBox1.DisplayMember = "FullName";
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var u = new User()
+            {
+                LastName = textBox2.Text,
+                FirstName = textBox1.Text
+            };
+            users.Add(u);
         }
     }
 }
