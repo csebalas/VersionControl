72b8071c20ace0f14dcd23518bc3467c746bcff5
Balázs Cseke <csebalas2003@gmail.com>
2024-04-21 19:40:59 +0200

    XML feldogozása

--- MNB/MNB/Form1.cs
+++ MNB/MNB/Form1.cs
@@ -6,23 +6,27 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace MNB
 {
     public partial class Form1 : Form
     {
         public BindingList<RateData> rates = new BindingList<RateData>();
+        private string result;
         public Form1()
         {
             InitializeComponent();
             CallWebService();
             dataGridView1.DataSource = rates;
+            Xmlupgrade();
         }
 
-        private void CallWebService()
+        public void CallWebService()
         {
             var mnbService = new MNBArfolyamServiceSoapClient();
             var request = new GetExchangeRatesRequestBody()
@@ -32,13 +36,42 @@ namespace MNB
                 endDate = "2020-06-30"
             };
             var response = mnbService.GetExchangeRates(request);
-            var result = response.GetExchangeRatesResult;
+            this.result = response.GetExchangeRatesResult;
             Console.WriteLine(result);
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        public void Xmlupgrade()
         {
-            List<string> list = new List<string>();
+            // XML document létrehozása és az aktuális XML szöveg betöltése
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+            // Végigmegünk a dokumentum fő elemének gyermekein
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                // Létrehozzuk az adatsort és rögtön hozzáadjuk a listához
+                // Mivel ez egy referencia típusú változó, megtehetjük, hogy előbb adjuk a listához és csak később töltjük fel a tulajdonságait
+                var rate = new RateData();
+                rates.Add(rate);
+
+                // Dátum
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+                // Valuta
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+                // Érték
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    rate.Value = value / unit;
+            }
+        }
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            List<RateData> list = new List<RateData>();
         }
     }
 }
